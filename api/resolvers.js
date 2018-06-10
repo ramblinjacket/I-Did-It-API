9db01c832323a4e0fe62d2854045a1a6d6b90dee
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-10 19:45:54 -0400

    added new dependencies

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,6 +1,9 @@
+import uuidv4 from 'uuid/v4';
+
 // Some fake data
 const users = [
   {
+    id: 'e1ce3612-a526-43d8-a304-509adcb95bd2',
     firstName: 'Les',
     lastName: 'Barchard',
     email: 'les.barchard@gmail.com',
@@ -8,6 +11,7 @@ const users = [
     image: 'https://lh5.googleusercontent.com/-H0IOkBtqLP4/AAAAAAAAAAI/AAAAAAAAAq0/M2OVNW09O8E/s96-c/photo.jpg',
   },
   {
+    id: '029c5723-0a58-4720-83a0-4467a1a06239',
     firstName: 'Travis',
     lastName: 'Massey',
     email: 'travis.massey@gmail.com',
@@ -18,12 +22,24 @@ const users = [
 
 export default {
   Query: {
-    users: () => users,
+    users: (root, args) => {
+      console.log(args.id);
+      const user = {
+        id: 'e1ce3612-a526-43d8-a304-509adcb95bd2',
+        firstName: 'Les',
+        lastName: 'Barchard',
+        email: 'les.barchard@gmail.com',
+        token: 'sdfsfddsf',
+        image: 'https://lh5.googleusercontent.com/-H0IOkBtqLP4/AAAAAAAAAAI/AAAAAAAAAq0/M2OVNW09O8E/s96-c/photo.jpg',
+      };
+      return user;
+    },
   },
 
   Mutation: {
     addUser: (root, args) => {
       const newUser = {
+        id: uuidv4(),
         firstName: args.firstName,
         lastName: args.lastName,
         email: args.email,
