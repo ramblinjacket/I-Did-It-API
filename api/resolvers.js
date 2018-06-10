d053a6c77cecbd320d99aecfec0ad14f7bca0801
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-10 08:34:42 -0400

    got a basic apollo graphql server in place

---
+++ api/resolvers.js
@@ -0,0 +1,15 @@
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+export default {
+  Query: { books: () => books },
+}
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+import resolvers from './api/resolvers';
+import typeDefs from './api/schema.gql';
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// Initialize the app
+const app = express();
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Go to http://localhost:3000/graphiql to run queries!');
+});
