d34286885113d1994678034941dc92ad442085a8
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-10 17:11:40 -0400

    lint cleanup

--- api/resolvers.js
+++ api/resolvers.js
@@ -5,30 +5,30 @@ const users = [
     lastName: 'Barchard',
     email: 'les.barchard@gmail.com',
     token: 'sdfsfddsf',
-    image: 'https://lh5.googleusercontent.com/-H0IOkBtqLP4/AAAAAAAAAAI/AAAAAAAAAq0/M2OVNW09O8E/s96-c/photo.jpg'
+    image: 'https://lh5.googleusercontent.com/-H0IOkBtqLP4/AAAAAAAAAAI/AAAAAAAAAq0/M2OVNW09O8E/s96-c/photo.jpg',
   },
   {
     firstName: 'Travis',
     lastName: 'Massey',
     email: 'travis.massey@gmail.com',
     token: '3234234',
-    image: 'https://lh6.googleusercontent.com/-i21fCaqZNjA/AAAAAAAAAAI/AAAAAAAAAD0/AYHn7_UQdXA/s92-c-k-no/photo.jpg'
+    image: 'https://lh6.googleusercontent.com/-i21fCaqZNjA/AAAAAAAAAAI/AAAAAAAAAD0/AYHn7_UQdXA/s92-c-k-no/photo.jpg',
   },
 ];
 
 export default {
   Query: {
-    users: () => users
+    users: () => users,
   },
 
   Mutation: {
     addUser: (root, args) => {
-      const newUser = { 
-        firstName: args.firstName, 
-        lastName: args.lastName, 
-        email: args.email, 
-        token: args.token, 
-        image: args.image 
+      const newUser = {
+        firstName: args.firstName,
+        lastName: args.lastName,
+        email: args.email,
+        token: args.token,
+        image: args.image,
       };
       users.push(newUser);
       return newUser;
