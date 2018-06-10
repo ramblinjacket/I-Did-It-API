24416e57662a30f83f1393db25f2f9aebe0107e7
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-06-10 17:13:41 -0400

    es lint

--- server.js
+++ server.js
@@ -20,6 +20,4 @@ app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+app.listen(3000, () => {});
