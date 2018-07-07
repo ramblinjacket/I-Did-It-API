e8ec6159cffd1cbdbbb7725c1d59c15431ef7975
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-07-07 09:03:58 -0400

    got select working

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,38 +1,43 @@
 import uuidv4 from 'uuid/v4';
-
-// Some fake data
-const users = [
-  {
-    id: 'e1ce3612-a526-43d8-a304-509adcb95bd2',
-    firstName: 'Les',
-    lastName: 'Barchard',
-    email: 'les.barchard@gmail.com',
-    token: 'sdfsfddsf',
-    image: 'https://lh5.googleusercontent.com/-H0IOkBtqLP4/AAAAAAAAAAI/AAAAAAAAAq0/M2OVNW09O8E/s96-c/photo.jpg',
-  },
-  {
-    id: '029c5723-0a58-4720-83a0-4467a1a06239',
-    firstName: 'Travis',
-    lastName: 'Massey',
-    email: 'travis.massey@gmail.com',
-    token: '3234234',
-    image: 'https://lh6.googleusercontent.com/-i21fCaqZNjA/AAAAAAAAAAI/AAAAAAAAAD0/AYHn7_UQdXA/s92-c-k-no/photo.jpg',
-  },
-];
+import db from '../db';
 
 export default {
   Query: {
-    users: (root, args) => {
-      console.log(args.id);
-      const user = {
-        id: 'e1ce3612-a526-43d8-a304-509adcb95bd2',
-        firstName: 'Les',
-        lastName: 'Barchard',
-        email: 'les.barchard@gmail.com',
-        token: 'sdfsfddsf',
-        image: 'https://lh5.googleusercontent.com/-H0IOkBtqLP4/AAAAAAAAAAI/AAAAAAAAAq0/M2OVNW09O8E/s96-c/photo.jpg',
-      };
-      return user;
+    users: async (root, args) => {
+      try {
+        const user = await
+        db.Users.findOne({
+          attributes: [
+            'Id',
+            'FirstName',
+            'LastName',
+            'Email',
+            'Token',
+            'Image',
+          ],
+          where: {
+            Id: args.id,
+          },
+        });
+        return {
+          id: user.dataValues.Id,
+          firstName: user.dataValues.FirstName,
+          lastName: user.dataValues.LastName,
+          email: user.dataValues.Email,
+          token: user.dataValues.Token,
+          image: user.dataValues.Image,
+        };
+      } catch (e) {
+        return {
+          id: '',
+          firstName: '',
+          lastName: '',
+          email: '',
+          token: '',
+          image: '',
+          state: '',
+        };
+      }
     },
   },
 
