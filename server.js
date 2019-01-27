2c403e2751a5490e6bccdb9799a36061cacedb3a
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2019-01-27 11:33:57 -0500

    fixed cors issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const schema = makeExecutableSchema({
 
 const app = express();
 
-app.use('*', cors({ origin: 'http://localhost:3000' }));
+app.use('*', cors({ origin: 'http://localhost:3001' }));
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
